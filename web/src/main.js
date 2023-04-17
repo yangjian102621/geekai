5e0bfc778b000697be60add5a1090e234f102eb6
RockYang <yangjian102621@gmail.com>
2023-04-17 09:56:02 +0800

    移除 vue-router URL 中的 # 号

--- web/src/main.js
+++ web/src/main.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHashHistory} from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import {createApp} from 'vue'
 import ElementPlus from "element-plus"
 import "element-plus/dist/index.css"
@@ -39,7 +39,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes: routes,
 })
 
