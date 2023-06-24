0b6940b12119399ba760b8c9a7e0c7ea6a0803df
RockYang <yangjian102621@gmail.com>
2023-06-25 06:53:22 +0800

    feat: chat list page for mobile is ready

--- web/src/views/mobile/Home.vue
+++ web/src/views/mobile/Home.vue
@@ -1,20 +1,24 @@
 <template>
-  <div>
-    <router-view/>
+  <van-config-provider :theme="theme">
+    <div class="mobile-home">
+      <router-view/>
 
-    <van-tabbar route v-model="active" @change="onChange">
-      <van-tabbar-item to="/mobile/chat" name="home" icon="chat-o"></van-tabbar-item>
-      <van-tabbar-item to="/mobile/setting" name="setting" icon="setting-o"></van-tabbar-item>
-      <van-tabbar-item to="/mobile/profile" name="profile" icon="user-o"></van-tabbar-item>
-    </van-tabbar>
+      <van-tabbar route v-model="active" @change="onChange">
+        <van-tabbar-item to="/mobile/chat" name="home" icon="chat-o"></van-tabbar-item>
+        <van-tabbar-item to="/mobile/setting" name="setting" icon="setting-o"></van-tabbar-item>
+        <van-tabbar-item to="/mobile/profile" name="profile" icon="user-o"></van-tabbar-item>
+      </van-tabbar>
 
-  </div>
+    </div>
+  </van-config-provider>
+
 </template>
 
 <script setup>
 import {ref} from "vue";
 
 const active = ref('home');
+const theme = ref("light")
 const onChange = (index) => {
   console.log(index)
   // showToast(`标签 ${index}`);
@@ -22,8 +26,32 @@ const onChange = (index) => {
 </script>
 
 <style lang="stylus">
+.mobile-home {
+  .container {
+    .van-nav-bar {
+      position fixed
+      width 100%
+    }
+
+    .content {
+      padding 46px 10px 0 10px;
+    }
+  }
+
+}
+
+// 黑色主题
+.van-theme-dark body {
+  background #1c1c1e
+}
+
 .van-toast--fail {
   background #fef0f0
   color #f56c6c
+}
+
+.van-nav-bar {
+  position fixed
+  width 100%
 }
 </style>
