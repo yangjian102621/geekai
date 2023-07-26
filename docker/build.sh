8aaff0af8992d15228a2cd82409137176621efd4
RockYang <yangjian102621@gmail.com>
2023-07-26 17:48:16 +0800

    chore: optimize build script, compress reward img

--- docker/build.sh
+++ docker/build.sh
@@ -13,18 +13,10 @@ cd ../docker
 
 # remove docker image if exists
 docker rmi -f registry.cn-hangzhou.aliyuncs.com/geekmaster/chatgpt-plus-go:$version
-docker rmi -f chatgpt-plus-go:$version
 # build docker image for chatgpt-plus-go
-docker build -t chatgpt-plus-go:$version -f dockerfile-api-go ../
+docker build -t registry.cn-hangzhou.aliyuncs.com/geekmaster/chatgpt-plus-go:$version -f dockerfile-api-go ../
 
 # build docker image for chatgpt-plus-vue
 docker rmi -f registry.cn-hangzhou.aliyuncs.com/geekmaster/chatgpt-plus-vue:$version
-docker rmi -f chatgpt-plus-vue:$version
-docker build --platform linux/amd64 -t chatgpt-plus-vue:$version -f dockerfile-vue ../
-
-# add tag for aliyum docker registry
-goImageId=`docker images |grep chatgpt-plus-go |grep $version |awk '{print $3}'`
-docker tag $goImageId registry.cn-hangzhou.aliyuncs.com/geekmaster/chatgpt-plus-go:$version
-vueImageId=`docker images |grep chatgpt-plus-vue |grep $version |awk '{print $3}'`
-docker tag $vueImageId registry.cn-hangzhou.aliyuncs.com/geekmaster/chatgpt-plus-vue:$version
+docker build --platform linux/amd64 -t registry.cn-hangzhou.aliyuncs.com/geekmaster/chatgpt-plus-vue:$version -f dockerfile-vue ../
 
