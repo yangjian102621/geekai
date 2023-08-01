8d39234fd05825f1ed47a035a9f95ef4a4f359b2
RockYang <yangjian102621@gmail.com>
2023-08-01 17:58:03 +0800

    fix: fixed bug for chat request's token reach the max token limit for OpenAI

--- api/core/types/chat.go
+++ api/core/types/chat.go
@@ -53,3 +53,10 @@ type ApiError struct {
 
 const PromptMsg = "prompt" // prompt message
 const ReplyMsg = "reply"   // reply message
+
+var ModelToTokens = map[string]int{
+	"gpt-3.5-turbo":     4096,
+	"gpt-3.5-turbo-16k": 16384,
+	"gpt-4":             8192,
+	"gpt-4-32k":         32768,
+}
