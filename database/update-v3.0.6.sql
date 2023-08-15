c8998ba29433ae47ddf7010cba6439d75f0113d5
RockYang <yangjian102621@gmail.com>
2023-08-15 22:53:32 +0800

    chore: update database sql file

--- database/update-v3.0.6.sql
+++ database/update-v3.0.6.sql
@@ -1,5 +1,5 @@
-ALTER TABLE `chatgpt_chat_history` ADD `use_context` TINYINT(1) NOT NULL COMMENT '是否允许作为上下文语料' AFTER `tokens`;
-ALTER TABLE `chatgpt_users` ADD `mobile` CHAR(11) NOT NULL COMMENT '手机号码' AFTER `username`;
+ALTER TABLE `chatgpt_chat_history` ADD `use_context` TINYINT(1) NULL DEFAULT NULL COMMENT '是否允许作为上下文语料' AFTER `tokens`;
+ALTER TABLE `chatgpt_users` ADD `mobile` CHAR(11) NULL DEFAULT NULL COMMENT '手机号码' AFTER `username`;
 
 CREATE TABLE `chatgpt_rewards` (
                                    `id` int NOT NULL,
@@ -18,7 +18,5 @@ ALTER TABLE `chatgpt_rewards`
 
 ALTER TABLE `chatgpt_rewards`
     MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
-update chatgpt_users set calls=0
 
 ALTER TABLE `chatgpt_rewards` ADD `user_id` INT(11) NOT NULL COMMENT '用户 ID' AFTER `id`;
