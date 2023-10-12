8b14e141d0b4458002260656d68c60ef4b2c900d
RockYang <yangjian102621@gmail.com>
2023-10-12 10:02:12 +0800

    opt: close websocket connection when finish a chat call for XunFei API

--- api/handler/chatimpl/chat_item_handler.go
+++ api/handler/chatimpl/chat_item_handler.go
@@ -7,6 +7,7 @@ import (
 	"chatplus/utils"
 	"chatplus/utils/resp"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 // List 获取会话列表
@@ -47,6 +48,95 @@ func (h *ChatHandler) List(c *gin.Context) {
 	resp.SUCCESS(c, items)
 }
 
+// Update 更新会话标题
+func (h *ChatHandler) Update(c *gin.Context) {
+	var data struct {
+		ChatId string `json:"chat_id"`
+		Title  string `json:"title"`
+	}
+	if err := c.ShouldBindJSON(&data); err != nil {
+		resp.ERROR(c, types.InvalidArgs)
+		return
+	}
+	res := h.db.Model(&model.ChatItem{}).Where("chat_id = ?", data.ChatId).UpdateColumn("title", data.Title)
+	if res.Error != nil {
+		resp.ERROR(c, "Failed to update database")
+		return
+	}
+
+	resp.SUCCESS(c, types.OkMsg)
+}
+
+// Clear 清空所有聊天记录
+func (h *ChatHandler) Clear(c *gin.Context) {
+	// 获取当前登录用户所有的聊天会话
+	user, err := utils.GetLoginUser(c, h.db)
+	if err != nil {
+		resp.NotAuth(c)
+		return
+	}
+
+	var chats []model.ChatItem
+	res := h.db.Where("user_id = ?", user.Id).Find(&chats)
+	if res.Error != nil {
+		resp.ERROR(c, "No chats found")
+		return
+	}
+
+	var chatIds = make([]string, 0)
+	for _, chat := range chats {
+		chatIds = append(chatIds, chat.ChatId)
+		// 清空会话上下文
+		h.App.ChatContexts.Delete(chat.ChatId)
+	}
+	err = h.db.Transaction(func(tx *gorm.DB) error {
+		res := h.db.Where("user_id =?", user.Id).Delete(&model.ChatItem{})
+		if res.Error != nil {
+			return res.Error
+		}
+
+		res = h.db.Where("user_id = ? AND chat_id IN ?", user.Id, chatIds).Delete(&model.HistoryMessage{})
+		if res.Error != nil {
+			return res.Error
+		}
+
+		// TODO: 是否要删除 MidJourney 绘画记录和图片文件？
+		return nil
+	})
+
+	if err != nil {
+		logger.Errorf("Error with delete chats: %+v", err)
+		resp.ERROR(c, "Failed to remove chat from database.")
+		return
+	}
+
+	resp.SUCCESS(c, types.OkMsg)
+}
+
+// History 获取聊天历史记录
+func (h *ChatHandler) History(c *gin.Context) {
+	chatId := c.Query("chat_id") // 会话 ID
+	var items []model.HistoryMessage
+	var messages = make([]vo.HistoryMessage, 0)
+	res := h.db.Where("chat_id = ?", chatId).Find(&items)
+	if res.Error != nil {
+		resp.ERROR(c, "No history message")
+		return
+	} else {
+		for _, item := range items {
+			var v vo.HistoryMessage
+			err := utils.CopyObject(item, &v)
+			v.CreatedAt = item.CreatedAt.Unix()
+			v.UpdatedAt = item.UpdatedAt.Unix()
+			if err == nil {
+				messages = append(messages, v)
+			}
+		}
+	}
+
+	resp.SUCCESS(c, messages)
+}
+
 // Remove 删除会话
 func (h *ChatHandler) Remove(c *gin.Context) {
 	chatId := h.GetTrim(c, "chat_id")
@@ -80,6 +170,7 @@ func (h *ChatHandler) Remove(c *gin.Context) {
 	resp.SUCCESS(c, types.OkMsg)
 }
 
+// Detail 对话详情，用户导出对话
 func (h *ChatHandler) Detail(c *gin.Context) {
 	chatId := h.GetTrim(c, "chat_id")
 	if utils.IsEmptyValue(chatId) {
