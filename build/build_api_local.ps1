c71b6a8e9e722cd7490740c1fa109eb1fcb0d162
kang.yang <xiaoyangkang@hotmail.com>
2024-01-13 19:45:07 +0800

    add build cmd
    1

---
+++ build/build_api_local.ps1
@@ -0,0 +1,13 @@
+# build go api program
+Set-Location -Path "..\api"
+
+# get os type and arch
+$os = & go env GOOS
+$arch = & go env GOARCH
+
+Write-Host "CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -o bin\chatgpt-plus-api main.go"
+
+$env:CGO_ENABLED="0"
+$env:GOOS=$os
+$env:GOARCH=$arch
+go build -o bin\chatgpt-plus-api main.go
