722ed128a156870195132e9a626716766d07c2e3
RockYang <yangjian102621@gmail.com>
2024-01-15 10:28:46 +0800

    opt: optimize vip recharge logic

--- api/service/mj/pool.go
+++ api/service/mj/pool.go
@@ -35,7 +35,7 @@ func NewServicePool(db *gorm.DB, redisCli *redis.Client, manager *oss.UploaderMa
 		if config.Enabled == false {
 			continue
 		}
-		config.ApiURL = "https://one-api.bltcy.top"
+		config.ApiURL = "https://api.chat-plus.net"
 		client := plus.NewClient(config)
 		name := fmt.Sprintf("mj-service-plus-%d", k)
 		servicePlus := plus.NewService(name, taskQueue, notifyQueue, 10, 600, db, client)
