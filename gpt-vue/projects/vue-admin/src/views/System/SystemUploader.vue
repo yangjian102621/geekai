557d547bf1ff2185b0a4c8dcefcd467a844a0644
廖彦棋 <liaoyq@pvc123.com>
2024-03-11 11:41:50 +0800

    feat(ui): 上传功能补充

--- gpt-vue/projects/vue-admin/src/views/System/SystemUploader.vue
+++ gpt-vue/projects/vue-admin/src/views/System/SystemUploader.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { computed, ref } from "vue";
+import { Message } from "@arco-design/web-vue";
 import type { UploadInstance, FileItem } from "@arco-design/web-vue";
 import { uploadUrl } from "@/http/config";
 
@@ -20,8 +21,10 @@ const uploadProps = computed<UploadInstance["$props"]>(() => ({
 }));
 
 const handleChange = (_, file: FileItem) => {
-  console.log(file.response);
-  console.log(file);
+  if (file?.response) {
+    emits("update:modelValue", file?.response?.data?.url);
+    Message.success("上传成功");
+  }
 };
 </script>
 <template>
