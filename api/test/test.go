4a8ff0ccf0b9e9678cfadf27d0c03ca80583de65
RockYang <yangjian102621@gmail.com>
2024-03-22 18:27:57 +0800

    chore: correct prompt messages

--- api/test/test.go
+++ api/test/test.go
@@ -17,8 +17,5 @@ type Student struct {
 
 func main() {
 
-	text := "2024-06-01 08:34:46"
-	fmt.Println(utils.Str2stamp(text))
-
-	fmt.Println(utils.Stamp2str(utils.Str2stamp(text)))
+	fmt.Println(utils.RandString(64))
 }
