1232c3cd9c2698804c298ea9a0ffed6db3c34fc5
RockYang <yangjian102621@gmail.com>
2024-05-04 21:30:29 +0800

    fix bug for license synchronize

--- api/core/app_server.go
+++ api/core/app_server.go
@@ -200,8 +200,10 @@ func authorizeMiddleware(s *AppServer, client *redis.Client) gin.HandlerFunc {
 
 func needLogin(c *gin.Context) bool {
 	if c.Request.URL.Path == "/api/user/login" ||
+		c.Request.URL.Path == "/api/user/logout" ||
 		c.Request.URL.Path == "/api/user/resetPass" ||
 		c.Request.URL.Path == "/api/admin/login" ||
+		c.Request.URL.Path == "/api/admin/logout" ||
 		c.Request.URL.Path == "/api/admin/login/captcha" ||
 		c.Request.URL.Path == "/api/user/register" ||
 		c.Request.URL.Path == "/api/user/session" ||
--- web/src/utils/http.js
+++ web/src/utils/http.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {getAdminToken, getSessionId, getUserToken} from "@/store/session";
+import {getAdminToken, getSessionId, getUserToken, removeAdminToken, removeUserToken} from "@/store/session";
 
 axios.defaults.timeout = 180000
 axios.defaults.baseURL = process.env.VUE_APP_API_HOST
@@ -22,9 +22,14 @@ axios.interceptors.response.use(
         let data = response.data;
         if (data.code === 0) {
             return response
-        } else {
-            return Promise.reject(response.data)
+        } else if (data.code === 400) {
+            if (response.request.responseURL.indexOf("/api/admin") !== -1) {
+                removeAdminToken()
+            } else {
+                removeUserToken()
+            }
         }
+            return Promise.reject(response.data)
     }, error => {
         return Promise.reject(error)
     })
