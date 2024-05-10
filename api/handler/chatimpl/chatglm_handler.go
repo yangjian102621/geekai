afb91a70236deff6e6aa42a0e04f986be75982a6
RockYang <yangjian102621@gmail.com>
2024-05-10 18:26:19 +0800

    optimize chat handler error handle code

--- api/handler/chatimpl/chatglm_handler.go
+++ api/handler/chatimpl/chatglm_handler.go
@@ -9,13 +9,14 @@ package chatimpl
 
 import (
 	"bufio"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"geekai/core/types"
 	"geekai/store/model"
 	"geekai/store/vo"
 	"geekai/utils"
-	"context"
-	"encoding/json"
-	"fmt"
 	"github.com/golang-jwt/jwt/v5"
 	"html/template"
 	"io"
@@ -45,14 +46,8 @@ func (h *ChatHandler) sendChatGLMMessage(
 			logger.Info("用户取消了请求：", prompt)
 			return nil
 		} else if strings.Contains(err.Error(), "no available key") {
-			utils.ReplyMessage(ws, "抱歉😔😔😔，系统已经没有可用的 API KEY，请联系管理员！")
-			return nil
-		} else {
-			logger.Error(err)
+			return errors.New("抱歉😔😔😔，系统已经没有可用的 API KEY，请联系管理员！")
 		}
-
-		utils.ReplyMessage(ws, ErrorMsg)
-		utils.ReplyMessage(ws, ErrImg)
 		return err
 	} else {
 		defer response.Body.Close()
