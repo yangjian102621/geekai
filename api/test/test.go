717b137a6d61bb33735c0e3fc65cf571dafb3882
RockYang <yangjian102621@gmail.com>
2024-05-22 18:15:06 +0800

    chore: use config value for order pay timeout

--- api/test/test.go
+++ api/test/test.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"geekai/utils"
 	"fmt"
+	"net/url"
 )
 
 func main() {
-	text := "https://nk.img.r9it.com/chatgpt-plus/1712709360012445.png 请简单描述一下这幅图上的内容 "
-	imgURL := utils.ExtractImgURL(text)
-	fmt.Println(imgURL)
+	text := "https://nk.img.r9it.com/chatgpt-plus/1712709360012445.png"
+	parse, _ := url.Parse(text)
+	fmt.Println(fmt.Sprintf("%s://%s", parse.Scheme, parse.Host))
 }
