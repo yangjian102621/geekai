17340ae0ac57defa21a6a3c6fb3fa008453660c3
RockYang <yangjian102621@gmail.com>
2024-05-30 09:59:44 +0800

    fixed bug for function call error None is not of type 'array'

--- api/core/types/function.go
+++ api/core/types/function.go
@@ -24,5 +24,4 @@ type Function struct {
 	Name        string                 `json:"name"`
 	Description string                 `json:"description"`
 	Parameters  map[string]interface{} `json:"parameters"`
-	Required    interface{}            `json:"required,omitempty"`
 }
