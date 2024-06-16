713001f2fcff920de25dfd2ed2a6723cbfae0854
dmd <1446961373@qq.com>
2024-06-16 21:10:39 +0800

    [feat] add 2 ollama model into chatgpt_chat_models.

--- database/update-v4.0.4.sql
+++ database/update-v4.0.4.sql
@@ -4,3 +4,5 @@ ALTER TABLE `chatgpt_dall_jobs` ADD `org_url` VARCHAR(400) NULL COMMENT '原图�
 ALTER TABLE `chatgpt_dall_jobs` DROP `task_id`;
 
 
+INSERT INTO `chatgpt_chat_models` (`platform`,`name`,`value`,`sort_num`,`enabled`,`power`,`temperature`,`max_tokens`,`max_context`,`open`,`key_id`,`created_at`,`updated_at`) VALUES ('Ollama','Qwen2-1.5B','qwen2:1.5b',22,1,0,1.0,2048,10240,1,0,'2024-06-10 15:04:54','2024-06-10 15:04:54');
+INSERT INTO `chatgpt_chat_models` (`platform`,`name`,`value`,`sort_num`,`enabled`,`power`,`temperature`,`max_tokens`,`max_context`,`open`,`key_id`,`created_at`,`updated_at`) VALUES ('Ollama','Qwen2-7B','qwen2:7b',22,1,0,1.0,2048,10240,1,0,'2024-06-10 15:04:54','2024-06-10 15:04:54');
