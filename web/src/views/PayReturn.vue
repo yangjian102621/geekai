f887a399121315a948729052036e3636c949c02e
RockYang <yangjian102621@gmail.com>
2024-09-18 22:24:05 +0800

    geek payment notify api is ready

---
+++ web/src/views/PayReturn.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+   支付回调
+  </div>
+</template>
+
+<script setup>
+import {useRouter} from "vue-router";
+
+const router = useRouter()
+console.log(router.currentRoute.value.query)
+window.close()
+</script>
