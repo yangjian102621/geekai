d5eeeea764ef53a18f213af0c1aebdd541d025f2
RockYang <yangjian102621@gmail.com>
2024-10-17 18:15:25 +0800

    优化充值产品定价逻辑，确保手机端和PC端显示的价格一致

--- web/src/views/Test.vue
+++ web/src/views/Test.vue
@@ -1,29 +1,12 @@
 <template>
   <div class="audio-chat-page">
     <el-button style="margin: 20px" type="primary" size="large" @click="connect()">开始语音对话</el-button>
-
-    <el-dialog v-model="showDialog" title="语音通话" :before-close="close">
-      <realtime-conversation  @close="showDialog = false" ref="conversationRef" :height="dialogHeight+'px'" />
-    </el-dialog>
   </div>
 </template>
 
 <script setup>
-import {nextTick, ref} from 'vue';
-import RealtimeConversation from "@/components/RealtimeConversation.vue";
-
-const showDialog = ref(false);
-const dialogHeight = ref(window.innerHeight - 75);
-const conversationRef = ref(null);
 const connect = () => {
-  showDialog.value = true;
-  nextTick(() => {
-    conversationRef.value.connect()
-  })
-}
-const close = () => {
-  showDialog.value = false;
-  conversationRef.value.hangUp()
+
 }
 </script>
 
